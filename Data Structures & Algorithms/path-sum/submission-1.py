b5836e519e1d6795f45ea284cea241ebda446427
Joey Lim <joeylimjunfeng@gmail.com>
2026-04-24 17:08:25 +0800

    Add: path-sum - submission-1

---
+++ Data Structures & Algorithms/path-sum/submission-1.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        # find all the path and add along
+        # sum them, then check if the target sum is reached
+        sum_val = 0
+
+        path = []
+
+        def canReachLeaf(root):
+
+            if not root:
+                return False
+
+            path.append(root.val)
+
+            # check that it is a leaf node to see if the path is correct
+            if not root.right and not root.left:
+
+                if sum(path)!=targetSum:
+                    path.pop()
+                    return False
+                else:
+                    return True
+
+            if canReachLeaf(root.left):
+                return True
+        
+            if canReachLeaf(root.right):
+                return True
+
+            # pop when it cant fulfil the condition
+            path.pop()
+            return False
+
+        return canReachLeaf(root)
+        
+        
