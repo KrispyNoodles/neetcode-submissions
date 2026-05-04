706a6d37379c2250353d77bd4f74ced7ffb29dba
Joey Lim <joeylimjunfeng@gmail.com>
2026-05-04 16:05:33 +0800

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # creating a mapCount
+        countMap = {}
+
+        for index, value in enumerate(nums):
+            
+            # checking if the answe exist
+            # and it is not the same
+            if (target-value) in countMap.values():
+                return [nums.index(target-value), index]
+                print('foubd')
+            
+            # adding in
+            if index not in countMap:
+
+                # index has to be the key because it is unique
+                countMap[index] = value
