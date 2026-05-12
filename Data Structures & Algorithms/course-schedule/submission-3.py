98e6475f66668b71cc6b25a9b615b9bcb097f2c5
Joey Lim <joeylimjunfeng@gmail.com>
2026-05-12 14:51:04 +0800

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,54 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        adjList = {}
+
+        for i in range(numCourses):
+            adjList[i]=[]
+
+        # create an adjList from the prerequisites
+        for start, dstn in prerequisites:
+
+            # then add the dstn to the start
+            adjList[start].append(dstn)
+        
+        visitSet = set()
+
+        # base cases
+        # you dont need take any course
+        if adjList == {}:
+            return True
+
+
+        def dfs(course):
+
+            # if we visit the node again
+            if course in visitSet:
+                return False
+
+            # if pre-requisite have nothing in the array then we can return True
+            if adjList[course] == []:
+                return True
+
+            # now we visit it
+            visitSet.add(course)
+
+            # looking through its neighbours
+            for neighbours in adjList[course]:
+                
+                # if false already above then can just immediately skip
+                if dfs(neighbours) == False:
+                    return False
+
+            # backtrack?
+            visitSet.remove(course)
+            # reassign, so when it reaches, there again we alreayd know that it is accessible
+            adjList[course] = []
+            return True
+
+        for course in range(numCourses):
+            if dfs(course)==False:
+                return False
+
+        # runs till the end
+        return True
