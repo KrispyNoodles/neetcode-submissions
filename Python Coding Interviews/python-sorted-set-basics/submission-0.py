c449bef5d950ba30707ac0e9ec6f72724e9cf7d0
Joey Lim <joeylimjunfeng@gmail.com>
2026-05-13 17:26:23 +0800

    Add: python-sorted-set-basics - submission-0

---
+++ Python Coding Interviews/python-sorted-set-basics/submission-0.py
@@ -0,0 +1,25 @@
+from typing import List
+from sortedcontainers import SortedSet
+
+
+def get_first_three(sorted_set: SortedSet[int], nums1: List[int], nums2: List[int]) -> List[int]:
+
+    # add nums 1 into the set
+    for num in nums1:
+        sorted_set.add(num)
+
+    # remove values in nums2
+    for value in nums2:
+
+        # discard doenst through an error if it is not in the set
+        sorted_set.discard(value)
+
+    return sorted_set[:3]
+    pass
+
+
+# do not modify below this line
+print(get_first_three(SortedSet(), [1, 2, 3], [4]))
+print(get_first_three(SortedSet([1, 4, 7, 2, 8, 9]), [10], [1, 7, 2]))
+print(get_first_three(SortedSet([1, 2, 3, 7]), [], [4, 5, 6]))
+print(get_first_three(SortedSet([1, 2, 3, 4, 5, 6, 7, 8, 9]), [10, 11, 12], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
