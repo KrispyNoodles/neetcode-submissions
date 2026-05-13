f19b25a349b65e3f0fd1533b3814a48abd95e442
Joey Lim <joeylimjunfeng@gmail.com>
2026-05-13 16:39:55 +0800

    Add: python-heap-pop - submission-0

---
+++ Python Coding Interviews/python-heap-pop/submission-0.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+
+    answer = []
+    
+    while heap:
+        pop_val = heapq.heappop(heap)
+        answer.append(pop_val)
+    
+    return answer
+    pass
+
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
