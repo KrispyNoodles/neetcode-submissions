9ab618e7e398adc3ee9c80f048b4983cba78e1c4
Joey Lim <joeylimjunfeng@gmail.com>
2026-05-15 12:42:07 +0800

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        left, right = 0, len(numbers)-1
+
+        while right>left:
+
+            sum_val = numbers[left] + numbers[right] 
+         
+            # checking if target is reached
+            if sum_val==target:
+                # 1-index
+                return [left+1, right+1]
+            
+            # if more means the right value is useless
+            elif sum_val>target:
+                right-=1
+            
+            else:
+                # this means that the small value is useless
+                left+=1
+
+        
