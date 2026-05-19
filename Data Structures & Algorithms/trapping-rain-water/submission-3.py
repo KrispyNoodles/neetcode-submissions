460c5fde1933a00cf2eafe42a078c8ab482885b9
Joey Lim <joeylimjunfeng@gmail.com>
2026-05-19 15:34:35 +0800

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        left, right = 0, len(height)-1
+
+        max_left = height[left]
+        max_right = height[right]
+
+        answer = 0
+
+        while right>left:
+
+            # if right is bigger, it means the water 
+            # for sure can be trapped on the left side
+
+            if height[right]>height[left]:
+                left+=1
+                # update the max height
+                max_left = max(max_left, height[left])
+                answer+= max_left-height[left]
+                
+            else:
+                right-=1
+                # update the max height
+                max_right = max(max_right, height[right])
+                answer+= max_right-height[right]
+                
+        return answer
+
+# time complexity of O(n)
