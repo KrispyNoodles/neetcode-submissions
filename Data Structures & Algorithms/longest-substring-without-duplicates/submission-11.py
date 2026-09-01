551101154be84fc30932871eb779062b27feb3bd
Joey Lim <joeylimjunfeng@gmail.com>
2026-09-01 13:43:31 +0800

    Add: longest-substring-without-duplicates - submission-11

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-11.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        left = 0
+        seen = set()
+        answer = 0
+
+        # left is the one moving?
+        for right in range(len(s)):
+            
+            if s[right] not in seen:
+
+                # add the left inside
+                seen.add(s[right])
+
+            else:
+                # if the new right is still inside keep removing all the elements from left inside until it is remove
+                while s[right] in seen:
+
+                    # remove the left and increment it
+                    seen.remove(s[left])
+                    left+=1
+
+                # ad in the s[right] after
+                seen.add(s[right])
+            
+            # update the answer
+            answer = max(answer, right-left+1)
+       
+        return answer
+            
